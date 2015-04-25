c366dd0a152184c635cd11c4040fb8b7cc5a6661
juandecarrion <juandeee@gmail.com>
2015-04-25 20:43:42 +0200

    Solved task 5

--- src/5-hits-from-ip/mapper.py
+++ src/5-hits-from-ip/mapper.py
@@ -12,5 +12,5 @@ for line in sys.stdin:
     data = line.strip().split()
     if len(data) == 10:
         ip, nothing, nothing, time, nothing, action_code, url, http_version, return_code, size = data
-        print "{0}\t{1}".format(url, size)
+        print "{0}\t{1}".format(ip, size)
 
