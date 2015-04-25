1f82d842196f1889b639491f311859db01374cb5
juandecarrion <juandeee@gmail.com>
2015-04-25 21:28:27 +0200

    Solved task 6

--- src/6-most-popular/mapper.py
+++ src/6-most-popular/mapper.py
@@ -8,9 +8,15 @@
 
 import sys
 
+BASE_URL = "http://www.the-associates.co.uk"
+
 for line in sys.stdin:
     data = line.strip().split()
     if len(data) == 10:
         ip, nothing, nothing, time, nothing, action_code, url, http_version, return_code, size = data
+
+        if url.find(BASE_URL) != -1:
+            url = url[len(BASE_URL):]
+
         print "{0}\t{1}".format(url, size)
 
