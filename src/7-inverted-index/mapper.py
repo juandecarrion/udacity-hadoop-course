f7720dd537745ff08abeb39433ccd3855b95dd2a
juandecarrion <juandeee@gmail.com>
2015-04-29 19:37:03 +0200

    Solved task 7

--- src/7-inverted-index/mapper.py
+++ src/7-inverted-index/mapper.py
@@ -8,50 +8,22 @@ import sys
 import csv
 
 
-def get_length(pair):
-    return pair[1]
-
-
 def mapper():
     reader = csv.reader(sys.stdin, delimiter='\t')
-    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
-
-    top_ten = []
 
     for line in reader:
+        import re
+        words = re.findall(r"[\w']+", line[4])
+        words = map(lambda x: x.lower(), words)
+        # if "fantastically" in words:
+        #     writer.writerow(line)
 
-        body = line[4]
-        length = len(body)
-
-        if len(top_ten) < 10 or top_ten[0][1] < length:
-            if len(top_ten) == 10:
-                top_ten.pop(0)
-            top_ten.append([line, length])
-            top_ten = sorted(top_ten, key=get_length)
-
-    for pair in top_ten:
-        writer.writerow(pair[0])
-
-
-test_text = """\"\"\t\"\"\t\"\"\t\"\"\t\"333\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"88888888\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"1\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"11111111111\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"1000000000\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"22\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"4444\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"666666\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"55555\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"999999999\"\t\"\"
-\"\"\t\"\"\t\"\"\t\"\"\t\"7777777\"\t\"\"
-"""
+        for word in words:
+            print word, '\t', line[0]
 
 
 # This function allows you to test the mapper with the provided test string
 def main():
-    import StringIO
-    sys.stdin = StringIO.StringIO(test_text)
     mapper()
-    sys.stdin = sys.__stdin__
 
 main()
